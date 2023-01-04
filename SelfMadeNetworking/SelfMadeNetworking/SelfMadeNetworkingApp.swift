24d380c2b3639dffd8f4258ffd7c06526efe45d8
@Roman Pronskyi <wildnoblebrute@gmail.com>
2023-01-04 21:33:46 +0200

    init base proj

---
+++ SelfMadeNetworking/SelfMadeNetworking/SelfMadeNetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelfMadeNetworkingApp.swift
+//  SelfMadeNetworking
+//
+//  Created by Roman Pronskyi on 04.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SelfMadeNetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
